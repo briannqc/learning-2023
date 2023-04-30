b2a530f3d72f2995f61d5d8c9dd108812ea84787
Brian NQC <brian.nqc@outlook.com>
2023-04-30 16:32:47 +0800

    [React] Dynamic greeting and inline style

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -7,17 +7,27 @@ import Image from 'react-bootstrap/Image';
 
 function App() {
   const author = "Brian NQC"
-  const currentYear = new Date().getFullYear()
-  const inlineStyleWorksButNotCommonlyUsed = {
-    border: "1px solid",
-    color: "darkseagreen"
+  const now = new Date()
+  const currentYear = now.getFullYear()
+
+  let greeting: string
+  let textColor: string
+  if (now.getHours() < 12) {
+    greeting = "Good morning!"
+    textColor = "red"
+  } else if (now.getHours() < 18) {
+    greeting = "Good afternoon!"
+    textColor = "green"
+  } else {
+    greeting = "Good evening!"
+    textColor = "blue"
   }
 
   return (
     <Container>
-      <h1 contentEditable="true" spellCheck="true">Music Player</h1>
+      <h1 style={{ color: textColor }}>Music Player, {greeting}</h1>
       <p>Created by {author}</p>
-      <p style={inlineStyleWorksButNotCommonlyUsed}>Copyright {currentYear}</p>
+      <p>Copyright {currentYear}</p>
       <Row>
         <Col lg={3} sm={4} xs={6}>
           <Image className="img-thumbnail thumbnail" src="https://picsum.photos/300" />
