48d2827f46d0891b715a36b27c1399c9aad5b3bf
Brian NQC <brian.nqc@outlook.com>
2023-04-30 16:01:06 +0800

    [React] Add react-bootstrap {Image, Row, Col}

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,6 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 
 function App() {
   const author = "Brian NQC"
@@ -8,9 +11,23 @@ function App() {
 
   return (
     <Container>
-      <h1>Music Player</h1>
+      <h1 contentEditable="true" spellCheck="true">Music Player</h1>
       <p>Created by {author}</p>
       <p>Copyright {currentYear}</p>
+      <Row>
+        <Col lg={3} sm={4} xs={6}>
+          <Image className="img-thumbnail thumbnail" src="https://picsum.photos/300" />
+        </Col>
+        <Col lg={3} sm={4} xs={6}>
+          <Image className="img-thumbnail thumbnail" src="https://picsum.photos/301" />
+        </Col>
+        <Col lg={3} sm={4} xs={6}>
+          <Image className="img-thumbnail thumbnail" src="https://picsum.photos/302" />
+        </Col>
+        <Col lg={3} sm={4} xs={6}>
+          <Image className="img-thumbnail thumbnail" src="https://picsum.photos/303" />
+        </Col>
+      </Row>
     </Container>
   )
 }
