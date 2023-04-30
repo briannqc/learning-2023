8b91cafc9ea56266135e9e14a9bb59f34e1c6e0f
Brian NQC <brian.nqc@outlook.com>
2023-04-30 23:34:30 +0800

    [React] Extract playlist/Thumbnail component

--- react-ts/src/components/playlist/CardSong.tsx
+++ react-ts/src/components/playlist/CardSong.tsx
@@ -1,7 +1,6 @@
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
+import Thumbnail from './Thumbnail';
 
-import './CardSong.css'
 
 type CardSongProps = {
     title: string
@@ -12,7 +11,7 @@ type CardSongProps = {
 function ListItem(props: CardSongProps) {
     return (
         <Col lg={3} sm={4} xs={6}>
-            <Image className="img-thumbnail thumbnail" src={props.thumbnailUrl} />
+            <Thumbnail src={props.thumbnailUrl} />
             <h6>{props.title}</h6>
             <p>{props.singer}</p>
         </Col>
---
+++ react-ts/src/components/playlist/Thumbnail.tsx
@@ -0,0 +1,12 @@
+import Image from 'react-bootstrap/Image';
+import './Thumbnail.css'
+
+type ThumbnailProps = {
+    src: string
+}
+
+function Thumbnail({ src }: ThumbnailProps) {
+    return <Image className="img-thumbnail thumbnail" src={src} />
+}
+
+export default Thumbnail;
