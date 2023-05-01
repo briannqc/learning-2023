b4935dfe7e966ecf1d53458e4c5acaf8ec2c130e
Brian NQC <brian.nqc@outlook.com>
2023-05-01 10:53:03 +0800

    [React] Render SongCard from data of songs list

--- react-ts/src/components/playlist/Playlist.tsx
+++ react-ts/src/components/playlist/Playlist.tsx
@@ -1,44 +1,16 @@
 import Row from 'react-bootstrap/Row';
 
 import CardSong from './CardSong'
+import songs from './songs'
 
 function Playlist() {
-
-    const songs = [
-        {
-            title: "Cheap Thrills",
-            singer: "Sia",
-            thumbnail: "https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/covers/d/1/d170e3fda75ec7afe19b1e01df3fce8a_1453722113.jpg",
-        },
-        {
-            title: "Dance Monkey",
-            singer: "Tones and I",
-            thumbnail: "https://media.cnn.com/api/v1/images/stellar/prod/201126135744-dance-monkey-shazam-trnd.jpg",
-        },
-        {
-            title: "Bam Bam",
-            singer: "Camila Cabello, Ed Sheeran",
-            thumbnail: "https://upload.wikimedia.org/wikipedia/en/4/43/Bam_Bam_featuring_Ed_Sheeran_%28Official_Single_Cover%29_by_Camila_Cabello.png",
-        },
-        {
-            title: "Coming Up Roses",
-            singer: "Keira Knightley",
-            thumbnail: "https://i1.sndcdn.com/artworks-000083225302-gfqebf-t500x500.jpg",
-        },
-        {
-            title: "Unstoppable",
-            singer: "Sia",
-            thumbnail: "https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/covers/d/1/d170e3fda75ec7afe19b1e01df3fce8a_1453722113.jpg",
-        },
-    ]
-
     return (
         <Row>
-            <CardSong title={songs[0].title} singer={songs[0].singer} thumbnailUrl={songs[0].thumbnail} />
-            <CardSong title={songs[1].title} singer={songs[1].singer} thumbnailUrl={songs[1].thumbnail} />
-            <CardSong title={songs[2].title} singer={songs[2].singer} thumbnailUrl={songs[2].thumbnail} />
-            <CardSong title={songs[3].title} singer={songs[3].singer} thumbnailUrl={songs[3].thumbnail} />
-            <CardSong title={songs[4].title} singer={songs[4].singer} thumbnailUrl={songs[4].thumbnail} />
+            {songs.map(song => <CardSong
+                key={song.id}
+                title={song.title}
+                singer={song.singer}
+                thumbnailUrl={song.thumbnailUrl} />)}
         </Row>
     )
 }
---
+++ react-ts/src/components/playlist/songs.ts
@@ -0,0 +1,196 @@
+const songs = [
+    {
+        title: 'Shape of You',
+        singer: 'Ed Sheeran',
+        thumbnailUrl: 'https://picsum.photos/250',
+        id: 0
+    },
+    {
+        title: 'Blinding Lights',
+        singer: 'The Weeknd',
+        thumbnailUrl: 'https://picsum.photos/251',
+        id: 1
+    },
+    {
+        title: 'Dance Monkey',
+        singer: 'Tones and I',
+        thumbnailUrl: 'https://picsum.photos/252',
+        id: 2
+    },
+    {
+        title: 'Levitating (feat. DaBaby)',
+        singer: 'Dua Lipa',
+        thumbnailUrl: 'https://picsum.photos/253',
+        id: 3
+    },
+    {
+        title: "Don't Start Now",
+        singer: 'Dua Lipa',
+        thumbnailUrl: 'https://picsum.photos/254',
+        id: 4
+    },
+    {
+        title: 'Watermelon Sugar',
+        singer: 'Harry Styles',
+        thumbnailUrl: 'https://picsum.photos/255',
+        id: 5
+    },
+    {
+        title: 'drivers license',
+        singer: 'Olivia Rodrigo',
+        thumbnailUrl: 'https://picsum.photos/256',
+        id: 6
+    },
+    {
+        title: 'Good 4 U',
+        singer: 'Olivia Rodrigo',
+        thumbnailUrl: 'https://picsum.photos/257',
+        id: 7
+    },
+    {
+        title: 'Montero (Call Me By Your Name)',
+        singer: 'Lil Nas X',
+        thumbnailUrl: 'https://picsum.photos/258',
+        id: 8
+    },
+    {
+        title: 'Kiss Me More (feat. SZA)',
+        singer: 'Doja Cat',
+        thumbnailUrl: 'https://picsum.photos/259',
+        id: 9
+    },
+    {
+        title: 'Save Your Tears (with Ariana Grande) (Remix)',
+        singer: 'The Weeknd',
+        thumbnailUrl: 'https://picsum.photos/260',
+        id: 10
+    },
+    {
+        title: 'Leave The Door Open',
+        singer: 'Silk Sonic, Bruno Mars, Anderson .Paak',
+        thumbnailUrl: 'https://picsum.photos/261',
+        id: 11
+    },
+    {
+        title: 'Deja Vu',
+        singer: 'Olivia Rodrigo',
+        thumbnailUrl: 'https://picsum.photos/262',
+        id: 12
+    },
+    {
+        title: 'Mood (feat. iann dior)',
+        singer: '24kGoldn',
+        thumbnailUrl: 'https://picsum.photos/263',
+        id: 13
+    },
+    {
+        title: 'positions',
+        singer: 'Ariana Grande',
+        thumbnailUrl: 'https://picsum.photos/264',
+        id: 14
+    },
+    {
+        title: 'Streets',
+        singer: 'Doja Cat',
+        thumbnailUrl: 'https://picsum.photos/265',
+        id: 15
+    },
+    {
+        title: 'Heat Waves',
+        singer: 'Glass Animals',
+        thumbnailUrl: 'https://picsum.photos/266',
+        id: 16
+    },
+    {
+        title: 'Up',
+        singer: 'Cardi B',
+        thumbnailUrl: 'https://picsum.photos/267',
+        id: 17
+    },
+    {
+        title: 'Someone You Loved',
+        singer: 'Lewis Capaldi',
+        thumbnailUrl: 'https://picsum.photos/268',
+        id: 18
+    },
+    {
+        title: 'Beautiful Mistakes (feat. Megan Thee Stallion)',
+        singer: 'Maroon 5',
+        thumbnailUrl: 'https://picsum.photos/269',
+        id: 19
+    },
+    {
+        title: 'Yonaguni',
+        singer: 'Bad Bunny',
+        thumbnailUrl: 'https://picsum.photos/270',
+        id: 20
+    },
+    {
+        title: 'Levitating (feat. DaBaby)',
+        singer: 'Dua Lipa',
+        thumbnailUrl: 'https://picsum.photos/271',
+        id: 21
+    },
+    {
+        title: 'Montero (Call Me By Your Name)',
+        singer: 'Lil Nas X',
+        thumbnailUrl: 'https://picsum.photos/272',
+        id: 22
+    },
+    {
+        title: 'Peaches (feat. Daniel Caesar & Giveon)',
+        singer: 'Justin Bieber',
+        thumbnailUrl: 'https://picsum.photos/273',
+        id: 23
+    },
+    {
+        title: 'Watermelon Sugar',
+        singer: 'Harry Styles',
+        thumbnailUrl: 'https://picsum.photos/274',
+        id: 24
+    },
+    {
+        title: 'Save Your Tears (with Ariana Grande) (Remix)',
+        singer: 'The Weeknd',
+        thumbnailUrl: 'https://picsum.photos/275',
+        id: 25
+    },
+    {
+        title: 'Driver License',
+        singer: 'Olivia Rodrigo',
+        thumbnailUrl: 'https://picsum.photos/276',
+        id: 26
+    },
+    {
+        title: 'Kiss Me More (feat. SZA)',
+        singer: 'Doja Cat',
+        thumbnailUrl: 'https://picsum.photos/277',
+        id: 27
+    },
+    {
+        title: 'Blinding Lights',
+        singer: 'The Weeknd',
+        thumbnailUrl: 'https://picsum.photos/278',
+        id: 28
+    },
+    {
+        title: 'The Business',
+        singer: 'Tiësto',
+        thumbnailUrl: 'https://picsum.photos/279',
+        id: 29
+    },
+    {
+        title: 'good 4 u',
+        singer: 'Olivia Rodrigo',
+        thumbnailUrl: 'https://picsum.photos/280',
+        id: 30
+    },
+    {
+        title: 'good 4 u',
+        singer: 'Olivia Rodrigo',
+        thumbnailUrl: 'https://picsum.photos/281',
+        id: 31
+    }
+]
+
+export default songs;
