28a9da5fc4684c521d3b0b7e5fec93d9ec5ecaa9
Brian NQC <brian.nqc@outlook.com>
2023-05-31 23:43:09 +0800

    My Home and RootLayout skeleton

---
+++ nextjs/app/layout.tsx
@@ -0,0 +1,25 @@
+import { PropsWithChildren } from "react";
+import '@styles/globals.css';
+
+export const metadata = {
+    title: "Promptopia",
+    discription: "Discover & Share AI Prompts",
+}
+
+const RootLayout = ({children}: PropsWithChildren) => {
+    return (
+        <html lang="en">
+            <body>
+                <div className="main">
+                    <div className="gradient" />
+                </div>
+
+                <main className="app">
+                    {children}
+                </main>
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout;
